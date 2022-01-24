8d4c36279e77dc5145923843a16c66559ae6f683
Iasmin <iasmindemagalhaes@gmail.com>
2022-01-24 16:39:26 -0400

    Inserção da pagina de bairros

--- geo_portal/assets/js/bairros.js
+++ geo_portal/assets/js/bairros.js
@@ -4,26 +4,6 @@
 
 var wfsLayer = new L.featureGroup();
 
-var url_geoserver = "http://localhost:8080/geoserver/wms?" 
-var url_geoserver_wfs = "http://localhost:8080/geoserver/ows?"
-
-// Get wms layer from geoserver
-
-var wmsLayer = new L.tileLayer.wms(url_geoserver,{
-    layers: 'bdgeo:bairro',
-    transparency: 'true',
-    format: 'image/svg',
-    opacity: 0.6, 
-    maxZoom: 20,
-    //cql_filter: "name='OLARIA'",
-    attribution: "Geo Portal"
-});
-
-
-//Get wfs layer from geoserver
-
-//http://localhost:8080/geoserver/bdgeo/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=bdgeo%3Abairro&outputFormat=application/json
-
 
 $.ajax('http://localhost:8080/geoserver/ows?',{
   type: 'GET',
@@ -38,28 +18,41 @@ $.ajax('http://localhost:8080/geoserver/ows?',{
   dataType: 'jsonp',
   jsonpCallback:'callback:handleJson',
   jsonp:'format_options'
-
  });
  
+  
   //Geojson style file
   var myStyle = {
-    fillColor: "#ff7800",
+    fillColor: "#E0A890",
     color: "#000",
     weight: 1,
     opacity: 1,
-    fillOpacity: 0.8,
+    fillOpacity: 0.4,
     attribution: "Geo Portal"
   }
+
+  
 // the ajax callback function
 function handleJson(data) {
     selectedArea = L.geoJson(data, {
       style: myStyle,
       onEachFeature: function(feature, layer) {
-        layer.bindPopup(`BAIRRO: ${feature.properties.name}`+`</br>`+`ZONA: ${feature.properties.zona}`)
+        //layer.bindPopup(`Bairro: ${feature.properties.name}`);
+        layer.bindTooltip(`Bairro ${feature.properties.name}`,{
+          direction: 'top',
+          permanent: false,
+          sticky: true,
+          offset: [10, 0],
+          opacity: 1,
+        
+        });
       }
+      
     }).addTo(wfsLayer);
   map.fitBounds(selectedArea.getBounds());
 }
+
+
 
 //atributos  do mapa
 
@@ -78,7 +71,7 @@ var map = L.map("map", {
     center: [-8.769659, -63.882688],
     zoom: 12,
     minZoom: 3,
-    layers: [cartodb]
+    layers: [cartodb, wfsLayer]
 })
 
 //web services layers
@@ -87,13 +80,13 @@ var baseLayers = {
     
     "Openstreet Map": osm,
     "CartoDB Light": cartodb
+    
 };
 
 //Overlay layer
 
 var overlayMaps = {
-    "Bairros (WMS)": wmsLayer,
-    "Bairros GeoJson":wfsLayer
+    "Bairros Distrito Sede":wfsLayer
 };
 
 //Add base layers
@@ -104,24 +97,3 @@ var controlLayers = L.control.layers(baseLayers, overlayMaps, {collapsed:false})
 
 L.control.scale({metric:true, imperial:false, maxWidth:100}).addTo(map);
 
-//Function to search keyword from WFS service
-
-function searchWFS(){
-  //Get value from inputbox
-  queryBox = document.getElementById("search-value").value;
-
-  if(!queryBox){
-    alert("Por favor, insira um valor válido!");
-    return false;
-  }
-
-  var cqlFilter = "cql_filter=%27name=%20OLARIA%20%27";
-  var url = "http://localhost:8080/geoserver/bdgeo/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=bdgeo%3Abairro&outputFormat=application/json&" + cqlFilter;
-  
-};
-
-//Function to clear results
-
-function clearResult(){
-  document.getElementById("search-value").value = "";
-};
